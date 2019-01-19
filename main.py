242d5ce2fb9d0856bbe269c6b58d7d268edeba90
gvantsats <gcucxashvili@gmail.com>
2019-01-19 13:55:33 +0400

    neural network implementation

--- main.py
+++ main.py
@@ -1,30 +1,55 @@
 import numpy as np
 import pandas as pd
 
-""" Handling data """
+class NN:
 
-""" Neural network implementation """
-NUM_ITERATIONS = 1000
+	def __init__(self , n_inputs):
+		self.label_names = ['უ', 'ყ', 'მ', 'შ', 'ძ', 'წ', 'ს', 'ხ', 'ლ', 'ჩ']
+		# learning info
+		self.n_iterations = 1000
+		# layer info
+		self.l_sizes = [n_inputs, 4, 4, 1]
+		# generating biases and weights on every hidden layer
+		self.biases = [np.random.randn(i, 1) for i in self.l_sizes[1:]]
+		self.weights = [np.random.randn(i, j) for i, j in zip(self.l_sizes[:-1], self.l_sizes[1:])]
 
-# Activation function
-def sigmoid(s):
-    return 1 / (np.exp(-1 * s) + 1)
+	# Activation function
+	def sigmoid(s):
+		return 1.0 / (np.exp(-1 * s) + 1.0)
 
-# Derivative of activation function
-def sigmoid_der(s):
-    return np.exp(-1 * s / ((1 + np.exp(-1 * s)) ** 2))
+	# Derivative of activation function
+	def sigmoid_der(s):
+		return sigmoid(s) * (1.0 - sigmoid(s))
 
-# Forward propagation
-def forward(X):
-	pass
+	# Forward propagation
+	def forward(self, data):
+		curr = data
+		for i in range(len(self.biases)):
+			bias = self.biases[i]
+			weight = self.weights[i]
+			mult = np.dot(weight , curr)
+			curr = self.sigmoid(mult + bias)
 
-# Backward propagation
-def backward(X, curr_y, y):
-	pass
+		return curr
 
-def training():
-	for i in range(NUM_ITERATIONS):
-   	curr_y = forward(X)
-   	backward(X, curr_y, y)
+	# Backward propagation
+	def backward(self, X, curr_y, y):
+		pass
 
-""" Getting predictions """
+	def training(self):
+		for i in range(self.n_iterations):
+			curr_y = forward(X)
+			backward(X, curr_y, y)
+
+	def classify(self , data):
+		best = float("-inf")
+		best_char = -1
+		for i in range(len(self.label_names)):
+			ans = self.forward(data)
+			if (ans > best):
+				best = ans
+				best_char = i
+		
+		return best_char
+
+	
