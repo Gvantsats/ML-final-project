8c4fec5810f493064a045177e776169f70e59a8f
Rogavactive <droga16@freeuni.edu.ge>
2019-02-02 18:31:53 +0400

    dataframe class minor optimization

--- data_frame.py
+++ data_frame.py
@@ -18,16 +18,21 @@ class DataObject:
 class TrainingDataFrame:
 
     data = {}
-
-    ROOT_DIR = "./data/ასოები/"
+    letters = []
 
     # If images are white on black, pass false as a first argument, please.
-    def __init__(self, black_on_white=True):
-        for letter in listdir(self.ROOT_DIR):
+    def __init__(self, black_on_white=True, root_dir="./data/ასოები/"):
+        self.add_data(root_dir, black_on_white)
+
+    # Taking parent (children of root_dir) folder names as labels, they should be only 1 letter long.
+    # Data should be in labeled letter folders.
+    # If images are white on black, pass false as a second argument, please.
+    def add_data(self, root_dir, black_on_white=True):
+        for letter in listdir(root_dir):
             if len(letter) > 1:
                 continue
-            for image_name in listdir(self.ROOT_DIR + letter):
-                img = cv2.imread(self.ROOT_DIR + letter + "/" + image_name)
+            for image_name in listdir(root_dir + letter):
+                img = cv2.imread(root_dir + letter + "/" + image_name)
                 if img is None:
                     print("wrong image path")
                 else:
@@ -36,6 +41,7 @@ class TrainingDataFrame:
                     resized_img = cv2.resize(img, dsize=(25, 25), interpolation=cv2.INTER_CUBIC)
                     if letter not in self.data:
                         self.data[letter] = []
+                        self.letters.append(letter)
                     self.data[letter].append(DataObject(resized_img))
 
     def get_random(self, letter):
@@ -45,12 +51,12 @@ class TrainingDataFrame:
         return self.data[letter]
 
     def get_letters(self):
-        return list(self.data.keys())
+        return self.letters
 
     def describe(self):
-        print("data contains " + str(len(self.data.keys())) + "letters, ")
+        print("data contains " + str(len(self.letters)) + "letters, ")
         total = 0
-        for letter in self.data.keys():
+        for letter in self.letters:
             amount = len(self.data[letter])
             total += amount
             print(str(amount) + " - " + letter + "'s.")
