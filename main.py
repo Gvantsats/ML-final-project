97c9d62aea1f187e8b747b1ef10463a17ce334b2
gvantsats <gcucxashvili@gmail.com>
2019-02-07 22:24:06 +0400

    fixed some bugs

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ class NN:
 	def __init__(self , n_inputs):
 		self.label_names = ['უ', 'ყ', 'მ', 'შ', 'ძ', 'წ', 'ს', 'ხ', 'ლ', 'ჩ']
 		# learning info
-		self.n_iterations = 1
+		self.n_iterations = 100000
 		# layer info
-		self.l_sizes = [n_inputs, 2, 1]
+		self.l_sizes = [n_inputs, 3 , 1]
 		self.n_layer = len(self.l_sizes)
 		# generating biases and weights on every hidden layer
 		self.biases = [np.random.randn(i, 1) for i in self.l_sizes[1:]]
 		self.weights = [np.random.randn(j, i) for i, j in zip(self.l_sizes[:-1], self.l_sizes[1:])]
 		# learning rate
-		self.l_rate = 0.01
+		self.l_rate = 0.001
 
 	# Activation function
 	def sigmoid(self, s):
@@ -26,20 +26,21 @@ class NN:
 
 	# Forward propagation
 	def forward(self, data):
+		data = data.reshape(data.shape[0] , 1)
 		curr = data
 		for i in range(len(self.biases)):
 			bias = self.biases[i]
 			weight = self.weights[i]
 			mult = np.dot(weight , curr)
 			curr = self.sigmoid(mult + bias)
-
+		
 		return curr
 
 	# Backward propagation
 	def backward(self, X, y):
+		X = X.reshape(X.shape[0] , 1)
 		biases_err = [np.zeros((i, 1)) for i in self.l_sizes[1:]]
 		weights_err = [np.zeros((j, i)) for i, j in zip(self.l_sizes[:-1], self.l_sizes[1:])]
-		
 		
 		# forward propagation while saving a and z values
 		a = [X]
@@ -63,8 +64,6 @@ class NN:
 			weights_err[-i] = np.dot(loss, a[-i - 1].transpose())
 			biases_err[-i] = loss
 
-		print (self.weights)
-		print(weights_err)
 		#update weights and biases
 		for i in range(len(self.biases)):
 			self.weights[i] -= self.l_rate * weights_err[i]
@@ -76,25 +75,6 @@ class NN:
 				X = data[j][0]
 				y = data[j][1]
 				self.backward(X , y)
-			
-				
-
-	def classify(self , data):
-		best = float("-inf")
-		best_char = -1
-		for i in range(len(self.label_names)):
-			ans = self.forward(data)
-			if (ans > best):
-				best = ans
-				best_char = i
-		
-		return best_char
 
 	
 if __name__ == "__main__":
-
-	xor_ex = [(np.array([0, 0]), np.array([0])), (np.array([0, 1]), np.array([1])), (np.array([1, 0]), np.array([1])), (np.array([1, 1]), ([0]))]
-
-	aq = NN(2)
-	aq.training(xor_ex)
-	print(d.forward([0 , 0]))
